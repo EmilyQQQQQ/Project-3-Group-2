94545add8c47b4eeb557bec8c18f53bae4c89691
kamal.farran.gil@gmail.com <kamalfarran.gil@gmail.com>
2023-12-04 21:06:24 -0500

    updates

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
 from flask import render_template 
+from flask import Markup
+from pymongo import MongoClient
+
 
 #################################################
 # Flask Setup
@@ -7,10 +10,22 @@ from flask import render_template
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
-
 def homepage():
     message = "Test"
     return render_template('homepage.html') 
+
+
+@app.route("/historical.html")
+def historical():
+
+    mongo = MongoClient(port=27017)
+    db = mongo['earthquakes']
+    query = {}
+    fields = {'_id': False}
+
+    earthquakes = db.hist_quakes.find(query, fields)
+    earthquakes_json = jsonify(list(earthquakes))
+    return render_template('historical.html', earthquake_list = earthquakes_json) 
     
 
 if __name__ == "__main__":
