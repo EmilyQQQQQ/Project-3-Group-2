14a5d178e89aceaf95c7b5c8480cc26d76dd75a7
kamal.farran.gil@gmail.com <kamalfarran.gil@gmail.com>
2023-12-06 17:34:52 -0500

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Markup
 import requests
 from pymongo import MongoClient
 import json
+from bs4 import BeautifulSoup
+import requests
 
 
 #################################################
@@ -33,20 +35,75 @@ def comparison():
 
     return render_template("comparison.html")
 
+
+@app.route("/getNews")
+def getNews():
+
+    newslist = []
+
+    url = "https://abcnews.go.com/alerts/earthquakes"
+
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    topics = soup.find("section", class_="Topics")
+
+    articles = topics.find_all("div", class_="ContentRoll__Headline")
+
+    for article in articles[0:15]:
+        title = article.find("a", class_="AnchorLink")["data-track-ctatext"]
+        link = article.find("a", class_="AnchorLink")["href"]
+
+        newslist.append("<a href=\"" + link + "\">"+title+"</a>")
+
+    return newslist
+
 @app.route("/getData")
 def getData():
+    
+    #Get Arguments
+    minMagnitude = request.args.get("minMagnitude", type=int)
+    maxMagnitude = request.args.get("maxMagnitude", type=int)
+    minYear = request.args.get("minYear", type=int)
+    maxYear = request.args.get("maxYear", type=int)
+    country = request.args.get("country", type=str)
 
+
+    #Specify Mongo connection
     mongo = MongoClient(port=27017)
     db = mongo['earthquakes']
-    query = {}
     fields = {'_id': 0}
 
-    earthquakes = db.hist_quakes.find(query, fields).limit(3)
+    #Prepare query critiera based on arguments
+    query = {}
+    if minMagnitude is None:
+        minMagnitude = 1
+    if maxMagnitude is None:
+        maxMagnitude = 9.5
+    query['eq_primary'] = {"$gte": minMagnitude, "$lte": maxMagnitude}
+
+    if minYear is None:
+        minYear = -2150
+    if maxYear is None:
+        maxYear = 2020
+    query['year'] = {"$gte": minYear, "$lte": maxYear}
+
+    if country:
+        country = country.upper()
+        query['country'] = country
+    
+    #Sort by magniude desc, date desc
+    sort = [('eq_primary', -1), ('year', -1), ('month', -1), ('day', -1)]
+
+    #Get data from the DB
+    earthquakes = db.hist_quakes.find(query, fields).sort(sort).limit(500)
     earthquakes = list(earthquakes)
 
+    #Return list of earthquakes
     return earthquakes
     
 
+    
     
 @app.route("/getLatestEarthquakes", methods=["GET"])
 def get_latest_earthquakes():
